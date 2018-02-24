dbc13a123d60ab04b00b6bb51c98ca1bb531340e
ysteven13 <ysteven13@gmail.com>
2018-02-24 15:54:46 -0600

    added processing support

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
-public class Main {
+import processing.core.*;
+public class Main extends PApplet {
 
     public static void main(String[] args) {
+        PApplet.main("Main");
 	// write your code here
+    }
+    public void settings() {
+        size (500,500);
+
+    }
+    public void setup() {
+    }
+    public void draw() {
+
     }
 }
